38b765a1f24867bdf7d07bdb44daf40117ab018b
Rebecca Zee <ryz283@gmail.com>
2021-07-21 15:21:46 -0400

    Toggling input

--- my-app/src/components/Item.js
+++ my-app/src/components/Item.js
@@ -4,12 +4,17 @@ function Item(props) {
   const { item } = props;
   const [isChecked, setIsChecked] = useState(false);
 
+  const handleChecking = () => {
+    if (isChecked) setIsChecked(false);
+    else setIsChecked(true);
+  }
+
   return (
     <div>
       <input
         type="checkbox"
         checked={isChecked}
-        onChange={() => setIsChecked(true)}
+        onChange={() => handleChecking()}
       />
       <label>{item}</label>
     </div >
